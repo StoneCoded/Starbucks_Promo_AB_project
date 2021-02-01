011d95cda7daf92492386b0fc0fd5546e51b8d23
StoneCoded <ben.stone92@hotmail.com>
2021-02-01 16:19:58 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
-# Starbucks_Promo_Model_project
- test project set by Udacity/Starbucks 
+# Starbucks Promotion Model
+A test project set by Udacity/Starbucks to build a model for finding Customers
+who will spend $10 given a Promotion. This project was originally a take home
+assignment for prospective Starbucks employees.
+
+##Contents
+1. Files and Implementation
+2. Project Overview
+3. Results and Observations
+4. Licensing, Authors, and Acknowledgements
+
+##1. Files and Implementation
+1. `training.csv`
+- CSV containing training data for building model
+2. `Test.csv`
+- CSV containing test data for model evaluating
+3. `Starbucks.py`
+- Data Wrangling and Model Creation. The main bulk of my working.
+4. `test_results.py`
+- Contains functions for evaluation IRR and NIR of model
+
+The standard Anaconda distribution of Python 3 is all you will need to run this.
+
+##2. Project Overview
+
+A randomized experiment was conducted and the results are in `Training.csv`
+`Treatment` – Indicates if the customer was part of treatment or control
+`Purchase` – Indicates if the customer purchased the product
+`ID` – Customer ID
+`V1` to `V7` – features of the customer
+Cost of sending a Promotion: $0.15
+Revenue from purchase of product: $10 (There is only one product)
+
+Questions:
+1.	Analyze the results of the experiment and identify the effect of the
+  Treatment on product purchase and Net Incremental Revenue
+2.	Build a model to select the best customers to target that maximizes the
+  Incremental Response Rate and Net Incremental Revenue.
+Deliverables:
+1.	Score the ‘Test.csv’ using the model and select the best customers and share
+  the customer ID’s as  csv file
+2.	Explain briefly the approach used in a separate document and also share the
+  code that can be executed to reproduce results.
+
+Incremental Response Rate:
+ (# of Purchasers In Treated) / Total # of customers in Treated  
+ - (# of Purchasers In Control) / Total # of customers in Control
+
+Net Incremental Revenue:
+ [(# of Purchasers in Treated *$10) – (# of Treated Customers *$0.15)]  
+ -  [# of Purchasers in Control * $10]
+
+##3. Results and Observations
+
+Model 1 in `Starbucks.py` performed suprisingly well for being a somewhat simple
+model. With an IRR of 0.0183 and NIR of 290.50, this a much more optimistic model
+than the given example. Whilst working I noticed a much higher NIR (above 400)
+could be achieved by pure chance of the random sample choice. Setting the
+random_state to 42 has given us the 290.50.
+
+Model 2 was a little more complex with an attempt at upsampling but either the
+method or the shear scale of the upsampling gave a rather lackluster result of
+an IRR of 0 and NIR of -0.75.
+
+There are many many more ways to play with this data and I no doubt will return
+to this at some point to see if I can improve and refactor (which I certainly can)
+
+##4.Licensing, Authors, and Acknowledgements
+All data was provided by Udacity and Starbuck for the purpose of this project.
+
+Udacity:[Here](https://www.Udacity.com)
+Starbucks:[Here](https://www.Starbucks.com)
+
+Copyright 2021 Ben Stone
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ Starbucks.py
@@ -0,0 +1,183 @@
+from itertools import combinations
+from test_results import test_results, score
+import numpy as np
+import pandas as pd
+import scipy as sp
+import sklearn as sk
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.utils import resample
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+
+# # Data Wrangling
+
+# load in the data and have a quick gander at it
+train_data = pd.read_csv('./training.csv')
+print(train_data.dtypes)
+train_data.describe()
+
+
+purchase_data = train_data[train_data['purchase'] == 1].copy()
+non_purchase_data = train_data[train_data['purchase'] == 0].copy()
+
+print(f'Total Purchases: {purchase_data.shape[0]}')
+print(f'Total Non-Purchases: {non_purchase_data.shape[0]}')
+
+# Random selection of non purchase data to create a balanced training set
+random_npd = non_purchase_data.sample(purchase_data.shape[0],random_state = 42).copy()
+new_train_data = pd.concat([purchase_data, random_npd]).reset_index(drop=True)
+
+#set X variable and get dummy variables for each feature within
+X = new_train_data.iloc[:,3:].copy()
+X = pd.get_dummies(data=X, columns=['V1','V4', 'V5','V6','V7'], drop_first = True)
+y = new_train_data['purchase']
+#Split new_training_data into train and test values for model
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LogisticRegression()
+model.fit(X_train,y_train)
+y_pred = model.predict(X_test)
+print(confusion_matrix(y_test, y_pred))
+print('Accuracy: {:.2f}))'.format(model.score(X_test, y_test)))
+
+
+def promotion_strategy(df):
+    '''
+    INPUT
+    df - a dataframe with *only* the columns V1 - V7 (same as train_data)
+
+    OUTPUT
+    promotion_df - np.array with the values
+                   'Yes' or 'No' related to whether or not an
+                   individual should recieve a promotion
+                   should be the length of df.shape[0]
+
+    Ex:
+    INPUT: df
+
+    V1	V2	  V3	V4	V5	V6	V7
+    2	30	-1.1	1	1	3	2
+    3	32	-0.6	2	3	2	2
+    2	30	0.13	1	1	4	2
+
+    OUTPUT: promotion
+
+    array(['Yes', 'Yes', 'No'])
+    indicating the first two users would recieve the promotion and
+    the last should not.
+    '''
+    df = pd.get_dummies(df, columns=['V1','V4', 'V5','V6','V7'], drop_first=True)
+    predicts = model.predict(df)
+    pred_map = {0: "No", 1: "Yes"}
+    promotion = np.vectorize(pred_map.get)(predicts)
+
+    return promotion
+
+test_results(promotion_strategy)
+#test results are very promising.
+#With IRR of 0.0183 and NIR of 290.50, this a much more optimistic model
+
+# ## Model 2
+#
+# Attempt at a large upsample model based on purchases but trained on whether
+# or not there is a promotion and purchase.
+
+train_data = pd.read_csv('./training.csv')
+train_data.columns = ['ID', 'promotion', 'purchase', 'V1', 'V2', 'V3', 'V4',
+        'V5', 'V6','V7']
+train_data['pro_pur'] = train_data.apply(lambda x: 1 if
+                                         (x['promotion'] == 'Yes') &
+                                         (x['purchase'] == 1) else 0, axis=1)
+train_data['promotion'] = train_data['promotion'].apply(lambda x: 1 if x
+                                                        == 'Yes' else 0)
+train_data.head()
+
+
+def upsample(df):
+    '''
+    INPUT:
+    dataframe
+
+    OUTPUT:
+    Dataframe with resampled purchase values for bigger
+    and hopefully more balanced and precise training/prediction
+    '''
+    purchases = df[df.purchase == 1]
+    non_purchases = df[df.purchase == 0]
+
+    purchases_upsampled = resample(purchases, replace=True,
+                        # sample with replacement
+                        n_samples = len(non_purchases),
+                        # match number in majority class
+                        random_state = 42)
+                        # reproducible results
+
+    df = pd.concat([purchases, non_purchases])
+    return df
+
+new_train_data = upsample(train_data)
+
+#Split new_training_data into train and test values for model
+X = new_train_data.iloc[:,3:10].copy()
+y = new_train_data['pro_pur']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
+                                                    random_state=42)
+#Initiate and fit classifier
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+#predict and score model
+y_pred = model.predict(X_test)
+print(confusion_matrix(y_test, y_pred))
+print('Accuracy: {:.4f}))'.format(model.score(X_test, y_test)))
+#[[16755 0]
+#[152   0]] suggests this didn't go according to plan
+
+def promotion_strategy(df):
+    '''
+    INPUT
+    df - a dataframe with *only* the columns V1 - V7 (same as train_data)
+
+    OUTPUT
+    promotion_df - np.array with the values
+                   'Yes' or 'No' related to whether or not an
+                   individual should recieve a promotion
+                   should be the length of df.shape[0]
+
+    Ex:
+    INPUT: df
+
+    V1	V2	  V3	V4	V5	V6	V7
+    2	30	-1.1	1	1	3	2
+    3	32	-0.6	2	3	2	2
+    2	30	0.13	1	1	4	2
+
+    OUTPUT: promotion
+
+    array(['Yes', 'Yes', 'No'])
+    indicating the first two users would recieve the promotion and
+    the last should not.
+    '''
+    y_pred = model.predict(df)
+    pred_map = {0: "No", 1: "Yes"}
+    promotion = np.vectorize(pred_map.get)(y_pred)
+
+    return promotion
+
+
+# In[116]:
+
+
+# This will test your results, and provide you back some information
+# on how well your promotion_strategy will work in practice
+
+test_results(promotion_strategy)
+#With an IRR of 0 and NIR of -0.75 my initial suspicions that this approach was
+#flawed was justified.
